75282e216d5a1059e473d1a7ed088711ab34b44b
mphstar <bintang.simdig20@gmail.com>
2023-05-17 16:52:56 +0700

    pull from master

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '8778f98bd95099736181f783e432be47b17be95f',
+        'reference' => 'af02e98e7f0132ea703c1b0a38dcee683b7ff158',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -418,7 +418,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '8778f98bd95099736181f783e432be47b17be95f',
+            'reference' => 'af02e98e7f0132ea703c1b0a38dcee683b7ff158',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -859,8 +859,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
