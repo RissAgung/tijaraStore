7765b5e962952388602395360ad825bf5fe2b2af
morimana-cuk <mr716604@gmail.com>
2023-05-29 03:42:59 +0700

    perbaikan pegawai

--- app/Http/Controllers/pengeluaran_re_stock.php
+++ app/Http/Controllers/pengeluaran_re_stock.php
@@ -10,12 +10,15 @@ use App\Models\pengeluaran\pengeluaran_barang;
 use App\Models\pengeluaran\pengeluaran_pegawai;
 use App\Models\products\barang;
 use App\Models\suplaier\suplaier;
+use App\Models\supplier;
 use Carbon\Carbon;
+use Illuminate\Console\View\Components\Alert;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Collection;
 use Maatwebsite\Excel\Facades\Excel;
+
 
 class pengeluaran_re_stock extends Controller
 {
@@ -24,8 +27,8 @@ class pengeluaran_re_stock extends Controller
         $datafinal = function ($request1, $date) {
             if ($request1->input('search')) {
                 return pengeluaran::with('pengeluaran_pegawai.pegawai')
-                    ->join('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
-                    ->join('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
+                    ->leftjoin('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
+                    ->leftjoin('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
                     ->where('pegawai.nama', '=', $request1->search)
                     ->orWhere('kode_pengeluaran', '=', $request1->search)
                     ->paginate(5);
@@ -34,8 +37,8 @@ class pengeluaran_re_stock extends Controller
 
                 if ($data->type === 'harian') {
                     return pengeluaran::with('pengeluaran_pegawai.pegawai')
-                        ->join('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
-                        ->join('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
+                        ->leftjoin('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
+                        ->leftjoin('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
                         ->whereDate('tanggal', '=', $data->data)
                         ->paginate(5);
                 } elseif ($data->type === 'mingguan') {
@@ -43,29 +46,29 @@ class pengeluaran_re_stock extends Controller
                     $end_date = Carbon::parse((string) $data->data)->endOfWeek();
 
                     return pengeluaran::with('pengeluaran_pegawai.pegawai')
-                        ->join('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
-                        ->join('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
+                        ->leftjoin('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
+                        ->leftjoin('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
                         ->whereBetween('tanggal', [$start_date, $end_date])->paginate(5);
                 } elseif ($data->type === 'bulanan') {
                     $tahun = $data->data->tahun;
                     $bulan = $data->data->bulan;
                     return pengeluaran::with('pengeluaran_pegawai.pegawai')
-                        ->join('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
-                        ->join('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
+                        ->leftjoin('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
+                        ->leftjoin('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
                         ->whereMonth('tanggal', '=', $bulan)
                         ->whereYear('tanggal', '=', $tahun)->paginate(5);
                 } elseif ($data->type === 'tahunan') {
                     $tahun = $data->data->tahun;
                     return pengeluaran::with('pengeluaran_pegawai.pegawai')
-                        ->join('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
-                        ->join('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
+                        ->leftjoin('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
+                        ->leftjoin('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
                         ->whereYear('tanggal', '=', $tahun)->paginate(5);
                 } elseif ($data->type === 'range') {
                     $date_awal = $data->data->awal;
                     $date_akhir = $data->data->akhir;
                     return pengeluaran::with('pengeluaran_pegawai.pegawai')
-                        ->join('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
-                        ->join('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
+                        ->leftjoin('pengeluaran_pegawai', 'pengeluaran.detail_pengeluaran_pegawai', 'pengeluaran_pegawai.pegawai_pengeluaran')
+                        ->leftjoin('pegawai', 'pengeluaran_pegawai.kode_pegawai', 'pegawai.kode_pegawai')
                         ->whereBetween('tanggal', [$date_awal, $date_akhir])->paginate(5);
                 }
             } 
@@ -74,7 +77,7 @@ class pengeluaran_re_stock extends Controller
         };
         $barang = barang::all();
         $pegawai = pengeluaran::with('pengeluaran_pegawai.pegawai')->where('pengeluaran_pegawai.kode_pegawai', Auth::user()->kode_pegawai);
-        $suplaier = suplaier::all();
+        $suplaier = supplier::all();
         $data = $datafinal($request,$date);
 
         $combinedata = compact('data', 'barang', 'pegawai', 'suplaier');
@@ -121,31 +124,31 @@ class pengeluaran_re_stock extends Controller
             dd($th);
         }
 
-        alert()->success('Berhasil', 'Berhasil Menambahkan restock');
+        Alert()->success('Berhasil', 'Berhasil Menambahkan restock');
         return redirect()->route('halaman_restock');
     }
-    public function search(Request $request)
-    {
-        $search = $request->input('search');
-        //dd($search);
-        $pegawai = pengeluaran::with('pengeluaran_pegawai.pegawai');
-        if ($pegawai) {
-            $pegawai->where('kode_pengeluaran', 'like', '%' . $search . '%')->orWhere('pegawai.nama', 'like', '%' . $search . '%');
-            toast('ini data nya', 'success')
-                ->position('top')
-                ->autoClose(3000);
-            return view('pengeluaran.data_pengeluaran_mainR');
-        } else {
-            toast('gagal', 'error')
-                ->position('top')
-                ->autoClose(3000);
-        }
+    // public function search(Request $request)
+    // {
+    //     $search = $request->input('search');
+    //     //dd($search);
+    //     $pegawai = pengeluaran::with('pengeluaran_pegawai.pegawai');
+    //     if ($pegawai) {
+    //         $pegawai->where('kode_pengeluaran', 'like', '%' . $search . '%')->orWhere('pegawai.nama', 'like', '%' . $search . '%');
+    //         toast('ini data nya', 'success')
+    //             ->position('top')
+    //             ->autoClose(3000);
+    //         return view('pengeluaran.data_pengeluaran_mainR');
+    //     } else {
+    //         toast('gagal', 'error')
+    //             ->position('top')
+    //             ->autoClose(3000);
+    //     }
 
-        // $pegawai = pengeluaran::where('kode_pengeluaran', 'like', '%' . $search . '%')
-        //     ->orWhere('kode_pegawai', 'like', '%' . $search . '%')
-        //     ->get();
-        // return view('pegawai.data_pegawai', ['pegawai' => $pegawai]);
-    }
+    //     // $pegawai = pengeluaran::where('kode_pengeluaran', 'like', '%' . $search . '%')
+    //     //     ->orWhere('kode_pegawai', 'like', '%' . $search . '%')
+    //     //     ->get();
+    //     // return view('pegawai.data_pegawai', ['pegawai' => $pegawai]);
+    // }
 
     public function export(){
         return Excel::download(new re_stock(), 're-stock.xlsx');
